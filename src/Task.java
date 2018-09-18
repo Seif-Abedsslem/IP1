501454e5b75e8ef966af020b953df3f025164be1
Seif-abedsslem <abedsslem.seif@yahoo.fr>
2018-09-18 17:30:52 +0200

    add something

--- src/Task.java
+++ src/Task.java
@@ -1,19 +1,18 @@
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 public class Task {
 
-   String title;
-   String status;
-   String dateTask;
-   String project;
+  private String title;
+  private String status;
+  private String dateTask;
+  private String project;
 
    // This is the constructor of the class Task
-   public Task(String title) {
-      this.title = title;
-   }
-   
-   public Task(String title,String status , String project) {
+    public Task(String title,String status , String project) {
 		this.title=title;
 		this.status=status;
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/M/yyyy");
@@ -21,6 +20,15 @@ public class Task {
 		this.project=project;
 		
 	}
+   public String getTitleTask() {
+       return title;
+   }
+   public String getStatusTask() {
+       return status;
+   }
+   public String getProjectTask() {
+       return project;
+   }
 
    // Assign the status of the task  to the variable status.
    public void taskStatus(String taskStatus) {
@@ -46,6 +54,37 @@ public class Task {
       System.out.println("Date of the Task: " + dateTask );
       System.out.println("This task belong to this Project: " + project);
       System.out.println("");
+   }
+   
+   public static void addTask(Scanner scanner) 
+	{
+			
+			System.out.println("adding a new task");
+			System.out.println("Please Enter the Title of the Task:");
+			
+			String title = scanner.nextLine();
+			System.out.println("Please Enter the Status of the Task:");
+			
+			String status = scanner.nextLine();
+			System.out.println("Please Enter the Project of the Task:");
+			
+			
+			String project = scanner.nextLine();
+			
+			Task taskone = new Task (title,status,project);
+			taskone.printTasks();
+			System.out.println("Task Created");
+	}
+   
+   public static void printAllTasks() {
+	   
+       List<Task> Tasks = new ArrayList<>();
+       Tasks.add(new Task("Rajeev", "done","project1"));
+       Tasks.add(new Task("John", "done","project1"));
+       Tasks.add(new Task("Steve", "done","project1"));
+
+       Tasks.forEach(task -> {
+           System.out.println(task.getTitleTask());});
    }
    
    
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,66 +1,59 @@
 import java.util.Scanner;
 
-
-
 public class UserInterface {
 
-	public static void main(String[] args)
-	  {
-	    
-		    System.out.println("*************************************************************************");
-			System.out.println(">> Welcome to ToDoly                                                    *");
-			System.out.println(">> You have X tasks todo and Y tasks are done!                          *");
-			System.out.println(">> Pick an option:                                                      *");
-			System.out.println(">> (1) Show Task List (by date or project)                              *");
-			System.out.println(">> (2) Add New Task                                                     *");
-			System.out.println(">> (3) Edit Task (update, mark as done, remove)                         *");
-			System.out.println(">> (4) Save and Quit                                                    *");
-			System.out.println(">>                                                                      *");
-			System.out.println("*************************************************************************");
-			
-			
-		     
-		     
-		     Scanner scanner = new Scanner(System.in);
-		     int choice ;
-		    
-			
-			do {
-				choice = scanner.nextInt();
-		     
-		      switch(choice) {
-		         case 1 :
-		        	 System.out.println(">>you choose to Show Task List (by date or project)");
-		        	 
-		        	
-		            break;
-		         case 2 :
-		        	 System.out.println(">>you choose to Add New Task");
-		        	
-		         	break;
-		         case 3 :
-		        	 System.out.println(">>you choose to dit Task (update, mark as done, remove)");
-		        	 
-		            break;
-		         case 4 :
-		        	 System.out.println(">>you choose to save and Quit");
-		        	 
-		            break;
-		         
-		         default :
-		        	 System.out.println("***********************************************************************"); 
-		             System.out.println("You have chosen an invalid option");
-		             System.out.println("***********************************************************************");
-		             System.out.println("Please insert a number between 1 and 4");
-		             System.out.println("***********************************************************************");
-		             
-		      }}
-		      while(choice != 1 ||choice != 2 ||choice != 3 ||choice != 4 );
-			
-			
-				scanner.close();
-				
-		      
-	}
+	public static void main(String[] args) {
 
+		System.out.println("*************************************************************************");
+		System.out.println(">> Welcome to ToDoly                                                    *");
+		System.out.println(">> You have X tasks todo and Y tasks are done!                          *");
+		System.out.println(">> Pick an option:                                                      *");
+		System.out.println(">> (1) Show Task List (by date or project)                              *");
+		System.out.println(">> (2) Add New Task                                                     *");
+		System.out.println(">> (3) Edit Task (update, mark as done, remove)                         *");
+		System.out.println(">> (4) Save and Quit                                                    *");
+		System.out.println(">>                                                                      *");
+		System.out.println("*************************************************************************");
+
+		Scanner scan = new Scanner(System.in);
+		String choice;
+		int x = 2;
+
+		do {
+
+			choice = scan.nextLine();
+			x = Integer.parseInt(choice);
+
+			switch (x) {
+			case 1:
+				System.out.println(">>you choose to Show Task List (by date or project)");
+
+				break;
+			case 2:
+				System.out.println(">>you choose to Add New Task");
+				Task.addTask(scan);
+
+				break;
+			case 3:
+				System.out.println(">>you choose to edit Task (update, mark as done, remove)");
+
+				break;
+			case 4:
+				System.out.println(">>you choose to save and Quit");
+
+				break;
+
+			default:
+				System.out.println("***********************************************************************");
+				System.out.println("You have chosen an invalid option");
+				System.out.println("***********************************************************************");
+				System.out.println("Please insert a number between 1 and 4");
+				System.out.println("***********************************************************************");
+
+			}
+		}while (x!= 1 || x!= 2 ||x!= 3 ||x!= 4  );
+
+		scan.close();
+
+	}
 }

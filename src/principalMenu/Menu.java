04acc93d74c2fcce8613691595dbfe0035914f09
Seif-abedsslem <abedsslem.seif@yahoo.fr>
2018-10-11 20:24:28 +0200

    Modification of the class diagram

--- src/principalMenu/Menu.java
+++ src/principalMenu/Menu.java
@@ -2,10 +2,18 @@ package principalMenu;
 
 import taskManager.TaskManagment;
 
+/**
+ * @author tmp-sda-1172
+ *
+ */
 public class Menu {
 
+	
 	private static TaskManagment taskManagment = new TaskManagment();
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		taskManagment.MenuDisplay();
 	}
